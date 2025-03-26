b83be64761a312095fb7c4116216c6bcc96cf8a7
Yoshua Soto <yoshuasoto95dev@gmail.com>
2025-03-26 14:17:06 -0400

    version 1.0

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -11,4 +11,14 @@ urlpatterns = [
     path('leads/', views.leads, name='leads'),
     path('leads/edit_lead/<int:id>', views.edit_lead, name="edit_lead"),
     path('leads/delete_lead/<int:id>', views.delete_lead, name="delete_lead"),
+
+    path('users/', views.users, name="users"),
+    path('users/register_user/', views.register_user, name="register_user"),
+    path('users/edit_user/<int:id>', views.edit_user, name="edit_user"),
+    path('users/delete_user/<int:id>', views.delete_user, name="delete_user"),
+    path('users/change_password_user/<int:id>', views.change_password_user, name="change_password_user"),
+
+    path('analytics/', views.analytics, name="analytics"),
+
+    path('config_profile/', views.config_profile, name="config_profile"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -30,13 +30,13 @@ def change_password(request, user_id):
             if password1 == password2:
                 user.set_password(password1)
                 user.save()
-                messages.success(request, "Password changed successfully.")
+                messages.success(request, "Password Changed Successfully.")
                 return redirect('dashboard:profile')
             else:
-                messages.error(request, "Passwords do not match.")
+                messages.error(request, "Passwords do not Match.")
                 return redirect('dashboard:change_password', user_id=user_id)
         else:
-            messages.error(request, "Incorrect password.")
+            messages.error(request, "Incorrect Password.")
             return redirect('dashboard:change_password', user_id=user_id)
 
     return render(request, 'profile.html', {'user': user})
@@ -79,5 +79,120 @@ def edit_lead(request, id):
 def delete_lead(request, id):
     lead = get_object_or_404(Lead, id=id)
     lead.delete()
-    messages.success(request, "Lead deleted Successfully!!!")
+    messages.success(request, "Lead Deleted Successfully!!!")
     return redirect('dashboard:leads')
+
+# USERS TABLE
+# =============================================================== #
+
+@login_required
+def users(request):
+    users = User.objects.all()
+    context = { 'users': users}
+    return render(request, 'users.html', context)
+
+@login_required
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        is_superuser = request.POST.get('is_superuser')
+        is_active = request.POST.get('is_active')
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(
+                    username = username,
+                    email = email,
+                    first_name = first_name,
+                    last_name = last_name,
+                    is_superuser = is_superuser,
+                    is_active = is_active,
+                    password = password1)
+                user.save()
+                messages.success(request, "User Created Successfully!!!")
+                return redirect('dashboard:users')
+            except:
+                messages.error(request, "User Exists, Try Again!!!")
+                return redirect('dashboard:users')
+        else:
+            messages.error(request, "Passwords do not Match")
+            return redirect('dashboard:users')
+
+    return render(request, 'users.html')
+
+
+@login_required
+def edit_user(request, id):
+    user = get_object_or_404(User, id=id)
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        is_superuser = request.POST.get('is_superuser')
+        is_active = request.POST.get('is_active')
+
+        if username and email and first_name and last_name:
+            user.username = username
+            user.email = email
+            user.first_name = first_name
+            user.last_name = last_name
+
+            if is_superuser:
+                user.is_superuser = is_superuser
+            if is_active:
+                user.is_active = is_active
+        user.save()
+        messages.success(request, "User Updated Sucessfully!!!")
+        return redirect('dashboard:users')
+
+    return render(request, 'users.html')
+
+@login_required
+def delete_user(request, id):
+    user = get_object_or_404(User, id=id)
+    user.delete()
+    messages.error(request, "User Deleted Successfully!!!")
+    return redirect('dashboard:users')
+
+
+@login_required
+def change_password_user(request, id):
+    user = get_object_or_404(User, id=id)
+
+    if request.method == 'POST':
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            user.set_password(password1)
+            user.save()
+            messages.success(request, "Password Changed Successfully.")
+            return redirect('dashboard:users')
+        else:
+            messages.error(request, "Passwords do not Match.")
+            return redirect('dashboard:users')
+    else:
+        messages.error(request, "All Fields are Required")
+        return redirect('dashboard:users')
+    
+# ANALYTICS TABLE
+# =============================================================== #
+
+@login_required
+def analytics(request):
+    return render(request, 'analytics.html')
+
+
+# CONFIG
+# =============================================================== #
+
+@login_required
+def config_profile(request):
+    return render(request, 'config.html')
