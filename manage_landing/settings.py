18fdcf351c0b7dcd1f9eb2ae68a79dd2f86a42ba
Yoshua Soto <yoshuasoto95dev@gmail.com>
2025-03-27 19:25:32 -0400

    version live 1.5.1

--- manage_landing/settings.py
+++ manage_landing/settings.py
@@ -87,7 +87,11 @@ WSGI_APPLICATION = 'manage_landing.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
     # 'default': { 'ENGINE': 'django.db.backends.sqlite3',
     #             'NAME': BASE_DIR / 'db.sqlite3'
                 }
