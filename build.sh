58b2334604ebd9636e6a5a38ea83a324d9d3d1d1
Yoshua Soto <yoshuasoto95dev@gmail.com>
2025-03-27 19:51:21 -0400

    version live 1.5.5

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
 set -o errexit
 
+#!/bin/bash
+
+# Instalar las dependencias de Python
 # pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# Aplicar migraciones
 python manage.py migrate
-python manage.py runscript init_superuser
+
+# Crear superusuario automáticamente (si no existe)
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@admin.com', 'password123')
+EOF
+
+# Recopilar archivos estáticos
+python manage.py collectstatic --noinput
+
+# Iniciar el servidor
+# gunicorn manage_landing.wsgi:application --bind 0.0.0.0:8000
