6467e90f3087e128ab51f1640e3fc2faf2093736
Yoshua Soto <yoshuasoto95dev@gmail.com>
2025-03-27 20:20:47 -0400

    version live 1.6

--- build.sh
+++ build.sh
@@ -8,13 +8,6 @@ set -o errexit
 # Aplicar migraciones
 python manage.py migrate
 
-# Crear superusuario automáticamente (si no existe)
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@admin.com', 'password123')
-EOF
-
 # Recopilar archivos estáticos
 python manage.py collectstatic --noinput
 
--- manage_landing/settings.py
+++ manage_landing/settings.py
@@ -87,14 +87,14 @@ WSGI_APPLICATION = 'manage_landing.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    # 'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-    # 'default': { 'ENGINE': 'django.db.backends.sqlite3',
-    #             'NAME': BASE_DIR / 'db.sqlite3'
+        # default='postgresql://postgres:postgres@localhost:5432/mysite',
+        # conn_max_age=600)
+     'default': { 'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': BASE_DIR / 'db.sqlite3'
                 }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
